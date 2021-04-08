4551374eba827a67388f11de11407bc2c185138f
Nikolui333 <semenovnikolu@yandex.ru>
2021-04-08 11:30:00 +0300

    input

--- web-app/web/database/views.py
+++ web-app/web/database/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from .models import Articles
+from .forms import ArticlesFrom
+
 
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'database/news_home.html', {'news': news})
 # Create your views here.
+
+
+def create(request):
+    form = ArticlesFrom()
+
+    data = {
+        'form': form
+    }
+
+    return render(request, 'database/create.html', data)
